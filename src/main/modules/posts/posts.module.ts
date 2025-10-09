80643ca5bdec7b5ad721042df65ad6ed26eceb5f
RafaelVsc <rafaelvsc88@gmail.com>
2025-10-09 14:34:58 -0300

    refactor(posts): reorganizar imports e remover dependência de UserRepository no GetPostByIdUseCase

--- src/main/modules/posts/posts.module.ts
+++ src/main/modules/posts/posts.module.ts
@@ -1,29 +1,26 @@
 // src/main/modules/posts/posts.module.ts
-import { Router } from 'express';
-import postsRoutes from '@/interfaces/routes/posts-routes';
-import { CreatePostController } from '@/interfaces/http/controllers/posts/create-post-controller';
-import { ListAllPostsController } from '@/interfaces/http/controllers/posts/find-posts-controller';
-import { UpdatePostController } from '@/interfaces/http/controllers/posts/update-post-controller';
-import { DeletePostController } from '@/interfaces/http/controllers/posts/delete-post-controller';
-import { InMemoryPostRepository } from '@/infrastructure/database/in-memory-post-repository';
 import { CreatePostUseCase } from '@/application/usecases/posts/create-post-use-case';
-import { ListPostsUseCase } from '@/application/usecases/posts/list-posts-use-case';
+import { DeletePostUseCase } from '@/application/usecases/posts/delete-post-use-case';
 import { GetPostByIdUseCase } from '@/application/usecases/posts/get-posts-by-id-use-case';
+import { ListPostsUseCase } from '@/application/usecases/posts/list-posts-use-case';
 import { SearchPostsUseCase } from '@/application/usecases/posts/search-posts-use-case';
 import { UpdatePostUseCase } from '@/application/usecases/posts/update-post-use-case';
-import { DeletePostUseCase } from '@/application/usecases/posts/delete-post-use-case';
-import { UserRepository } from '@/domain/repositories/users/user-repository';
 import { PostRepository } from '@/domain/repositories/posts/post-repository';
+import { CreatePostController } from '@/interfaces/http/controllers/posts/create-post-controller';
+import { DeletePostController } from '@/interfaces/http/controllers/posts/delete-post-controller';
+import { ListAllPostsController } from '@/interfaces/http/controllers/posts/find-posts-controller';
+import { UpdatePostController } from '@/interfaces/http/controllers/posts/update-post-controller';
+import postsRoutes from '@/interfaces/routes/posts-routes';
+import { Router } from 'express';
 
 export function buildPostsModule(
-  userRepository: UserRepository,
   postRepository: PostRepository
 ): Router {
   // const postRepository = new InMemoryPostRepository();
 
   const createPostUseCase = new CreatePostUseCase(postRepository);
   const listPostsUseCase = new ListPostsUseCase(postRepository);
-  const getPostByIdUseCase = new GetPostByIdUseCase(postRepository, userRepository);
+  const getPostByIdUseCase = new GetPostByIdUseCase(postRepository);
   const searchPostsUseCase = new SearchPostsUseCase(postRepository);
   const updatePostUseCase = new UpdatePostUseCase(postRepository);
   const deletePostUseCase = new DeletePostUseCase(postRepository);
